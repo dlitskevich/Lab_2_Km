e2a52abf9d290eed9557c33ff394097b02cae288
Danila <danila.litskevich@gmail.com>
2019-04-28 18:49:28 +0300

    reDONE: works for any type

--- 21_lab_2_5.py
+++ 21_lab_2_5.py
@@ -1,16 +1,48 @@
 
 
 def cached(f):
+    """
+    main task function
+    memoizing values with given params of ANY type
+    :param f:
+    :return:
+    """
     memo = {}
 
     def helper(*args, **kwargs):
-        if (args, frozenset(kwargs.items())) not in memo:
+        frozen_arguments = frozen_unhashable((args, kwargs.items()))
+        if frozen_arguments not in memo:
             print("Value added to cache {} {}".format(args, kwargs))
-            memo[(args, frozenset(kwargs.items()))] = f(*args, **kwargs)
+            memo[frozen_arguments] = f(*args, **kwargs)
         else:
             print("Value from cache {0} {1}".format(args, kwargs))
-        return memo[(args, frozenset(kwargs.items()))]
+        return memo[frozen_arguments]
     return helper
+
+
+def frozen_unhashable(argument):
+    """
+    frozenset applied to almost everything unhashable
+    :param argument:
+    :return:
+    """
+    # to prevent iteration through string as array of chars
+    if isinstance(argument, str):
+        return argument
+
+    frozen_arguments = []
+    try:
+        for arg in argument:
+            frozen_arguments.append(frozen_unhashable(arg))
+    except TypeError:
+        # for leaves
+        try:
+            hash(argument)
+            return argument
+        except TypeError:
+            return frozenset(argument)
+    # no matter what iterable object it was
+    return frozenset(frozen_arguments)
 
 
 @cached
@@ -63,10 +95,12 @@ if __name__ == "__main__":
     # DO NOT use numbers over ~900 as RecursionError can occur
     fibonacci_order = 13
     # for demonstration only
+    # print(hash({1:2}))
+
     print("{}\n".format(recursive_fibonacci(fibonacci_order)))
 
     print("{}".format(fibonacci_tail_recursion(fibonacci_order)))
     print("{}\n".format(fibonacci_tail_recursion(fibonacci_order)))
 
-    print("{}".format(test(12, 345, key=1, key2=2)))
-    print("{}".format(test(12, 345, key2=2, key=1, )))
+    print("{}".format(test(12, 345, key={1: {2: {3: [1, 2]}}}, key2=2)))
+    print("{}".format(test(12, 345, key2=2, key={1: {2: {3: [1, 2]}}}, )))
