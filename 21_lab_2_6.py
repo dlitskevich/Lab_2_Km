ad9baf3cbc12d56b47bed451647db41335b53f87
Danila <danila.litskevich@gmail.com>
2019-05-08 16:32:16 +0300

    Probably, DONE (not sure)

--- 21_lab_2_6.py
+++ 21_lab_2_6.py
@@ -1,17 +1,31 @@
-class Range:
-    start_number = 0
-    before_number = 0
-    step = 1
 
+def objectify(passed_class):
+    """"""
+    object_of_class = passed_class()
+    return object_of_class
+
+@objectify
+class Range:
     def __init__(self):
         return
 
-    def __call__(self, before_num):
-        before_number = before_num
-        return before_number
+    def __call__(self, start_number, before_number=None, step=1):
+        if before_number is None:
+            current_number = 0
+            before_number = start_number
+        else:
+            current_number = start_number
+
+        range_list = []
+        while current_number < before_number:
+            range_list.append(current_number)
+            current_number += step
+        return range_list
+
 
 if __name__ == "__main__":
-    print(dir(range))
-    print(range.__call__(1, 6, 3))
+    print(Range(2, 100, 5))
+    print(Range(2, 6))
+    print(Range(2))
+    print(1 in Range(2))
 
-    print(Range(1))
