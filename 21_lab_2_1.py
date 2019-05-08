160cc34373eab002820254d9abee4ae52fe97c82
Danila <danila.litskevich@gmail.com>
2019-05-08 19:35:12 +0300

    Started

---
+++ 21_lab_2_1.py
@@ -0,0 +1,29 @@
+
+def ndarray(passed_class):
+    """"""
+    object_of_class = passed_class()
+    return object_of_class
+
+
+@ndarray
+class Vector:
+    def __init__(self, *):
+
+        return
+
+    def __call__(self, start_number, before_number=None, step=1):
+        if before_number is None:
+            current_number = 0
+            before_number = start_number
+        else:
+            current_number = start_number
+
+        range_list = []
+        while current_number < before_number:
+            range_list.append(current_number)
+            current_number += step
+        return range_list
+
+
+if __name__ == "__main__":
+    print(type(Vector))
