746ff2d77be335753298d3f87dd0a405b1540981
Danila <danila.litskevich@gmail.com>
2019-05-09 22:21:07 +0300

    TODO: add operations

---
+++ 21_lab_2_9.py
@@ -0,0 +1,31 @@
+
+class Value(object):
+    value = 0
+    commission = 10
+
+    def __get__(self, instance, owner):
+        return self.value, instance.commission
+
+    def __set__(self, instance, init_value):
+        self.value = init_value[0]
+        self.commission = init_value[1]
+
+
+class Account:
+    amount = Value()
+
+    def __init__(self, init_value=0, init_commission=10):
+        self.commission = init_commission
+        self.amount = (init_value, init_commission)
+        return
+
+    def __call__(self, operation=None, amount=None):
+        
+        return self.amount
+
+
+if __name__ == "__main__":
+    test_account = Account(1, 1)
+    test_an_account = Account(20, 3)
+    print(test_account())
+    print(test_an_account())
