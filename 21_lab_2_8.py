10c3eafac0dae2c7fdb0107e092ea1d7170dfff7
Danila <danila.litskevich@gmail.com>
2019-05-09 20:16:05 +0300

    Done as class decorator

--- 21_lab_2_8.py
+++ 21_lab_2_8.py
@@ -1,19 +1,25 @@
 
-def singleton_decorator(passed_class):
 
-    class Singleton:
-        def __init__(self, *args, **kwargs):
-            if not self.instance:
-                self.instance = passed_class(args, kwargs)
-            else:
-                print("Yep")
+class Singleton:
+    first_instance = 1
 
-        def __getattr__(self, name):
-            return getattr(self.wrapped, name)
+    def __init__(self, passed_class):
+        self.passed_class = passed_class
 
-    return Singleton
+    def __call__(self, *args):
+        if self.first_instance:
+            self.instance = self.passed_class(*args)
+            self.first_instance = 0
+            return self
+        else:
+            self.instance.__dict__ = self.passed_class(*args).__dict__
+            return self
+
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
 
 
+@Singleton
 class Test:
     value = 1
 
@@ -21,12 +27,21 @@ class Test:
         self.value = number
 
     def __str__(self):
-        return "{} : {}".format(object.__str__(self), self.value)
+        return "{} : {}".format(repr(self), self.value)
+
 
 if __name__ == "__main__":
     a = Test(2)
+    print(a)
     b = Test(3)
+    print(a)
+    print(b)
     c = Test(4)
     print(a)
     print(b)
     print(c)
+    d = Test(5)
+    print(a)
+    print(b)
+    print(c)
+    print(d)
