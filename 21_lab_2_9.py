ad3cbe0a4dceefff007b4bc6cbd018643b6d5e36
Danila <danila.litskevich@gmail.com>
2019-05-10 13:59:45 +0300

    Done

--- 21_lab_2_9.py
+++ 21_lab_2_9.py
@@ -1,26 +1,53 @@
 
 class Value(object):
-    value = 0
-    commission = 10
-
     def __get__(self, instance, owner):
-        return self.value, self.commission
+        return instance.value, instance.commission
 
     def __set__(self, instance, init_value):
-        self.value = init_value[0]
-        self.commission = init_value[1]
+        instance.value = init_value[0]
+        instance.commission = init_value[1]
 
 
 class Account:
-    amount = Value()
+    balance = Value()
 
     def __init__(self, init_value=0, init_commission=10):
-        self.amount = (init_value, init_commission)
+        self.balance = (init_value, init_commission)
         return
 
-    def __call__(self, operation=None, amount=None):
+    def __call__(self, operation=None, amount=0):
+        operations = {
+            None: ("None", str(self)),
+            "add":  ["self.add(amount)",
+                     "Balance change: +{} Commission was {}"
+                     "".format(amount, (self.balance[1]/100*amount))],
+            "subtract": ["self.add(-amount)",
+                         "Balance change: -{} Commission was {}"
+                         "".format(amount, (self.balance[1]/100*amount))],
+            "commission": ["self.change_commission(amount)",
+                           "Commission changed to {}%".format(amount)]
+        }
+        if operation in operations:
+            eval(operations[operation][0])
+            return operations[operation][1]
+        else:
+            return "No operation's been found"
 
-        return self.amount
+    def __str__(self):
+        return "Account's balance: {} Account's commission: {}%".format(
+            self.balance[0], self.balance[1]
+        )
+
+    def add(self, amount):
+        balance = self.balance[0]
+        commission = self.balance[1]
+        changed_balance = balance+amount-abs(amount)*commission/100
+        self.balance = (changed_balance, commission)
+        return
+
+    def change_commission(self, commission):
+        self.balance = (self.balance[0], commission)
+        return
 
 
 if __name__ == "__main__":
@@ -28,5 +55,14 @@ if __name__ == "__main__":
     test_an_account = Account(20, 3)
 
     print(test_account())
+    print(test_account("commission", 10))
+    print(test_account)
+    print(test_account("add", 10))
+    print(test_account)
+    print(test_account("subtract", 10))
+    print(test_account)
+
+    print()
     print(test_an_account.__dict__)
     print(test_an_account())
+    print(test_account())
