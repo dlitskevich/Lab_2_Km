722ce02ff579abe617626141fb41156818b91554
Danila <danila.litskevich@gmail.com>
2019-05-10 09:57:48 +0300

    python 3

--- 21_lab_2_9.py
+++ 21_lab_2_9.py
@@ -4,7 +4,7 @@ class Value(object):
     commission = 10
 
     def __get__(self, instance, owner):
-        return self.value, instance.commission
+        return self.value, self.commission
 
     def __set__(self, instance, init_value):
         self.value = init_value[0]
@@ -15,17 +15,18 @@ class Account:
     amount = Value()
 
     def __init__(self, init_value=0, init_commission=10):
-        self.commission = init_commission
         self.amount = (init_value, init_commission)
         return
 
     def __call__(self, operation=None, amount=None):
-        
+
         return self.amount
 
 
 if __name__ == "__main__":
     test_account = Account(1, 1)
     test_an_account = Account(20, 3)
+
     print(test_account())
+    print(test_an_account.__dict__)
     print(test_an_account())
