332d9def49dfebb4d5c1f092c3a92628d01cbe27
Danila <danila.litskevich@gmail.com>
2019-05-10 11:08:22 +0300

    python 3

--- 21_lab_2_1.py
+++ 21_lab_2_1.py
@@ -1,92 +1,85 @@
 
-def objectify(passed_class):
-    """"""
-    class Array:
-        def __init__(self, coord_list):
-            self.vector = passed_class(coord_list)
-            self.vector.coord = coord_list
-            self.vector.dimension = len(coord_list)
-
-        def __getattr__(self, name):
-            return getattr(self.vector, name)
-
-        def __str__(self):
-            return "array({})".format(self.vector.coord)
-
-        def __add__(self, other):
-            if not self.vector.dimension == other.dimension:
-                raise ValueError("To sum up dimensions must be equal")
-
-            new_coord = []
-            for coord, an_coord in zip(self.vector.coord, other.coord):
-                new_coord.append(coord + an_coord)
-
-            return Vector(new_coord)
-
-        def __mul__(self, other):
-            if not isinstance(other, (int, float)):
-                raise TypeError("Multiplication is defined for numbers only")
-
-            new_coord = []
-            for coord in self.vector.coord:
-                new_coord.append(coord * other)
-
-            return Vector(new_coord)
-        __rmul__ = __mul__
-
-        def __sub__(self, other):
-            if not self.vector.dimension == other.dimension:
-                raise ValueError("To subtract dimensions must be equal")
-
-            new_coord = []
-            for coord, an_coord in zip(self.vector.coord, other.coord):
-                new_coord.append(coord - an_coord)
-
-            return Vector(new_coord)
-        __rsub__ = __sub__
-
-        def __cmp__(self, other):
-            if not self.vector.dimension == other.dimension:
-                return True
-
-            for coord, an_coord in zip(self.vector.coord, other.coord):
-                if not coord == an_coord:
-                    return True
-            return False
-
-        def __len__(self):
-            return self.vector.dimension
-
-        def __getitem__(self, index):
-            return self.vector.coord[index]
-
-        def __mod__(self, other):
-            if not self.vector.dimension == other.dimension:
-                raise ValueError("To Multiplication dimensions must be equal")
-
-            product = 0
-            for coord, an_coord in zip(self.vector.coord, other.coord):
-                product += coord * an_coord
-
-            return product
-    return Array
-
 
 class NdArray(object):
     def __get__(self, instance, owner):
-        return instance.coord
+        return instance.coord_list
 
     def __set__(self, instance, value):
-        instance.coord = value
+        instance.coord_list = value
 
 
-@objectify
 class Vector:
     coord = NdArray()
-    dimension = 0
 
-    def __init__(self, *args):
+    def __init__(self, coord_list):
+        self.coord = coord_list
+        self.dimension = len(coord_list)
         return
+
+    def __getattr__(self, name):
+        return getattr(self, name)
+
+    def __str__(self):
+        return "array({})".format(self.coord)
+
+    def __add__(self, other):
+        if not self.dimension == other.dimension:
+            raise ValueError("To sum up dimensions must be equal")
+
+        new_coord = []
+        for coord, an_coord in zip(self.coord, other.coord):
+            new_coord.append(coord + an_coord)
+
+        return Vector(new_coord)
+
+    def __mul__(self, other):
+        if not isinstance(other, (int, float)):
+            raise TypeError("Multiplication is defined for numbers only")
+
+        new_coord = []
+        for coord in self.coord:
+            new_coord.append(coord * other)
+
+        return Vector(new_coord)
+
+    __rmul__ = __mul__
+
+    def __sub__(self, other):
+        if not self.dimension == other.dimension:
+            raise ValueError("To subtract dimensions must be equal")
+
+        new_coord = []
+        for coord, an_coord in zip(self.coord, other.coord):
+            new_coord.append(coord - an_coord)
+
+        return Vector(new_coord)
+
+    __rsub__ = __sub__
+
+    def __eq__(self, other):
+        if not self.dimension == other.dimension:
+            return False
+
+        for coord, an_coord in zip(self.coord, other.coord):
+            if not coord == an_coord:
+                return False
+        return True
+
+    def __len__(self):
+        return self.dimension
+
+    def __getitem__(self, index):
+        return self.coord[index]
+
+    def __mod__(self, other):
+        if not self.dimension == other.dimension:
+            raise ValueError("To Multiplication dimensions must be equal")
+
+        product = 0
+        for coord, an_coord in zip(self.coord, other.coord):
+            product += coord * an_coord
+
+        return product
 
 
 if __name__ == "__main__":
@@ -95,9 +88,14 @@ if __name__ == "__main__":
     cmp_array = Vector([1, 2, 3])
     number = -12.3
 
+    print(array.__dict__)
+    print(array.coord)
+    print(an_array.coord)
+
     print("What we have:\n"
           "{} : {}\n"
-          "{} : {}\n".format(array, array.coord, an_array, an_array.coord))
+          "{} : {}\n".format(repr(array), array.coord,
+                             repr(an_array), an_array.coord))
 
     print("Addition:\n"
           "{}\n"
@@ -127,4 +125,3 @@ if __name__ == "__main__":
           "{}\n"
           "{}\n".format(array % an_array, array % array))
 
-    print(array)
