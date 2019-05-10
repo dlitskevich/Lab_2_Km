a78eb0d39a65133966ab07840fd1a8378d1ef2b1
Danila <danila.litskevich@gmail.com>
2019-05-10 12:03:44 +0300

    python 3

--- 21_lab_2_8.py
+++ 21_lab_2_8.py
@@ -1,22 +1,34 @@
 
 
 class Singleton:
-    first_instance = 1
+    __first_instance = 1
 
     def __init__(self, passed_class):
-        self.passed_class = passed_class
+        self.__passed_class = passed_class
 
     def __call__(self, *args):
-        if self.first_instance:
-            self.instance = self.passed_class(*args)
-            self.first_instance = 0
-            return self
+        if self.__first_instance:
+            self.__instance = self.__passed_class(*args)
+            self.__first_instance = 0
+            return self.__instance
         else:
-            self.instance.__dict__ = self.passed_class(*args).__dict__
-            return self
+            self.__instance.__dict__ = self.__passed_class(*args).__dict__
+            return self.__instance
+    """
+    def __getattribute__(self, item):
+        exception_list = [
+            "_Singleton__first_instance",
+            "_Singleton__passed_class",
+            "_Singleton__instance"
+        ]
+        if item in exception_list:
+            return object.__getattribute__(self, item)
 
+        return object.__getattribute__(self.__instance, item)
+    """
+    
     def __getattr__(self, name):
-        return getattr(self.instance, name)
+        return getattr(self.__instance, name)
 
 
 @Singleton
@@ -27,12 +39,26 @@ class Test:
         self.value = number
 
     def __str__(self):
-        return "{} : {}".format(repr(self), self.value)
+        return "{} : {}".format(object.__str__(self), self.value)
+
+
+@Singleton
+class TestAn:
+    value = 1
+
+    def __init__(self, number):
+        self.value = number
+
+    def __call__(self, *args, **kwargs):
+        return 123
+
+    def __repr__(self):
+        return "{} : {}".format(object.__repr__(self), self.value)
 
 
 if __name__ == "__main__":
     a = Test(2)
-    print(a)
+    print(a.__dict__)
     b = Test(3)
     print(a)
     print(b)
@@ -41,6 +67,22 @@ if __name__ == "__main__":
     print(b)
     print(c)
     d = Test(5)
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+
+    print()
+    a = TestAn(2)
+    print(a(1))
+    b = TestAn(3)
+    print(a)
+    print(b)
+    c = TestAn(4)
+    print(a)
+    print(b)
+    print(c)
+    d = TestAn(5)
     print(a)
     print(b)
     print(c)
