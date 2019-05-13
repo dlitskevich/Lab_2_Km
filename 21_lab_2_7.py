6174bd3b126cee4b0c044fb280be3c1793636c49
Danila <danila.litskevich@gmail.com>
2019-05-13 12:21:59 +0300

    Saved

--- 21_lab_2_7.py
+++ 21_lab_2_7.py
@@ -9,6 +9,7 @@ class Sequence:
             raise
 
     def __iter__(self):
+        self.__iterable = iter(self.__iterable_saved)
         return self
 
     def __next__(self):
@@ -19,16 +20,20 @@ class Sequence:
             return next(self.__iterable)
 
     def filter(self, function):
-        self.__iterable_saved = list(filter(function, self.__iterable_saved))
+        filtered = []
+        for item in self.__iterable_saved:
+            if function(item):
+                filtered.append(item)
+        self.__iterable_saved = filtered
         self.__iterable = iter(self.__iterable_saved)
-        return next(self)
+        return self
 
 
 if __name__ == "__main__":
     test_sequence = Sequence(range(5, 40))
     test_an_sequence = Sequence(range(5, 9))
-    print(test_sequence.filter(lambda a: 7 < a < 26))
-
+    print(test_sequence.filter(lambda a: 9 < a < 12))
+    
     stop = 0
     for test_item in test_sequence:
         if stop > 100:
