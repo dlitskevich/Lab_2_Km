f46dbd065727f1422adb414d4bfb456d57b29e04
Danila <danila.litskevich@gmail.com>
2019-05-17 21:31:53 +0300

    TODO: add other methods

--- 21_lab_2_6.py
+++ 21_lab_2_6.py
@@ -1,36 +1,71 @@
 
-def objectify(passed_class):
-    """"""
-    object_of_class = passed_class()
-    return object_of_class
+
+class RangeIterator:
+    def __init__(self, start, stop, step):
+        self.stop = stop
+        self.start = start
+        self.step = step
+        self.current = self.start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.current += self.step
+        if self.start < self.stop:
+            if self.step < 0 \
+                    or self.current > self.stop >= 0 \
+                    or self.current < self.stop <= 0:
+                raise StopIteration
+        else:
+            if self.step > 0 \
+                    or self.current < self.stop >= 0 \
+                    or self.current > self.stop <= 0:
+                raise StopIteration
+        return self.current - self.step
 
 
-@objectify
 class Range:
-    def __init__(self, *args):
+    def __init__(self, start, *args):
+        quantity = len(args)
+        if quantity > 2:
+            raise TypeError("Range expected at most 3 arguments, got {}"
+                            .format(quantity+1))
+        if quantity == 0:
+            self.stop = start
+            self.start = 0
+        else:
+            self.stop = args[0]
+            self.start = start
+        if quantity == 1 or quantity == 0:
+            self.step = 1
+        else:
+            self.step = args[1]
+            if self.step == 0:
+                raise ValueError("Range() arg 3 must not be zero")
         return
 
-    def __call__(self, start_number, before_number=None, step=1):
-        if before_number is None:
-            current_number = 0
-            before_number = start_number
-        else:
-            current_number = start_number
+    def __iter__(self):
+        return RangeIterator(self.start, self.stop, self.step)
 
-        range_list = []
-        while current_number < before_number:
-            range_list.append(current_number)
-            current_number += step
-        return range_list
+    def __str__(self):
+        if self.step == 1:
+            return "Range({}, {})".format(self.start, self.stop)
+        else:
+            return "Range({}, {}, {})" \
+                   "".format(self.start, self.stop, self.step)
 
 
 if __name__ == "__main__":
-    print(Range(2, 100, 5))
-    print(Range(2, 6))
-    print(Range(2))
-    print(1 in Range(2))
+    print(range(200, -1))
+    print(iter(range(1)))
+    print(repr(range))
+    print(repr(Range))
+    print(range(1, ))
+    print(Range(1, ))
+    print(dir(iter(range(1))))
+    print([x for x in range(49, 90)])
+    print([x for x in Range(49, 90)])
 
-    print(next(iter(Range(2))))
-    print()
-    for item in Range(6, 65, 7):
-        print(item)
+
+
