05ae73a0a35770833e289faf587d2c52bd09d948
Danila <danila.litskevich@gmail.com>
2019-05-26 12:16:52 +0300

    Going to do with MC

--- 21_lab_2_3.py
+++ 21_lab_2_3.py
@@ -1,13 +1,62 @@
+import sys
+
+
+class LoggerMCL(type):
+    def __repr__(self):
+        string = "Logs:\n"
+        for log in self.logs:
+            string += "{} Result: {} {}\n".format(log[0], log[1], log[2])
+        return string
+
+
+class LoggerTech(object, metaclass=LoggerMCL):
+    logs = []
+
+    @classmethod
+    def log(cls, method):
+        # better use wraps from functools
+        def logging(*args, **kwargs):
+            result = method(*args, **kwargs)
+            cls.logs.append((method.__name__, result, (args, kwargs)))
+            print(type(cls))
+
+        return logging
 
 
 class Logger:
-    pass
+    """
+    print(sys._getframe().f_back.f_code)
+    print(sys._getframe().f_code.co_names)
+    print(sys._getframe().f_code.co_name)
+    """
+
+    def __getattribute__(self, item):
+        method = super().__getattribute__(item)
+        # types.MethodType
+        if callable(method):
+            setattr(self, item, LoggerTech.log(object.__getattribute__(self, item)))
+        return method
+
+    def __call__(self, *args, **kwargs):
+        print(123)
+        return 12
+
+    # @LoggerTech.log
+    def method1(self, arg=1):
+        print(Logger.__subclasses__())
+        return 1, 2
 
 
-class Test:
+class Test(Logger):
     def recycle(self):
         return "recycled"
 
 
 if __name__ == "__main__":
+    a = Logger()
+    print(a.__dict__)
+    a.method1(2)
+    print(a.__dict__)
     print()
+
+    print(LoggerTech)
