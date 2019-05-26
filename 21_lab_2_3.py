6da0ddb29c8501258ac05aed1e6e09e2f38bb74e
Danila <danila.litskevich@gmail.com>
2019-05-26 13:30:15 +0300

    probably DONE

--- 21_lab_2_3.py
+++ 21_lab_2_3.py
@@ -1,62 +1,73 @@
-import sys
 
 
-class LoggerMCL(type):
-    def __repr__(self):
+class LoggerMCS(type):
+    logs = []
+
+    @classmethod
+    def log(mcs, method):
+        # better use wraps from functools
+        def logging(*args, **kwargs):
+            result = method(*args, **kwargs)
+            mcs.logs.append((method.__name__, result, (args, kwargs)))
+            return result
+        return logging
+
+    def __new__(mcs, name, bases, dct):
+        """
+        if not hasattr(mcs, 'logs'):
+            mcs.logs = []
+        """
+        newattrs = {}
+        for attrname, attrvalue in dct.items():
+            if callable(attrvalue):
+                newattrs[attrname] = mcs.log(attrvalue)
+            else:
+                newattrs[attrname] = attrvalue
+
+        return super().__new__(mcs, name, bases, newattrs)
+
+    def __init__(cls, name, bases, dct):
+        super().__init__(name, bases, dct)
+
+    def __str__(self):
         string = "Logs:\n"
-        for log in self.logs:
+        for log in LoggerMCS.logs:
             string += "{} Result: {} {}\n".format(log[0], log[1], log[2])
         return string
 
 
-class LoggerTech(object, metaclass=LoggerMCL):
-    logs = []
-
-    @classmethod
-    def log(cls, method):
-        # better use wraps from functools
-        def logging(*args, **kwargs):
-            result = method(*args, **kwargs)
-            cls.logs.append((method.__name__, result, (args, kwargs)))
-            print(type(cls))
-
-        return logging
-
-
-class Logger:
-    """
-    print(sys._getframe().f_back.f_code)
-    print(sys._getframe().f_code.co_names)
-    print(sys._getframe().f_code.co_name)
-    """
-
-    def __getattribute__(self, item):
-        method = super().__getattribute__(item)
-        # types.MethodType
-        if callable(method):
-            setattr(self, item, LoggerTech.log(object.__getattribute__(self, item)))
-        return method
+class Logger(metaclass=LoggerMCS):
 
     def __call__(self, *args, **kwargs):
         print(123)
         return 12
 
-    # @LoggerTech.log
-    def method1(self, arg=1):
-        print(Logger.__subclasses__())
-        return 1, 2
+    def method1(self, arg):
+        print("method 1 called with {}".format(arg))
+        return 1, 2, arg
+
+
+def decorator(func):
+    def change(*args, **kwargs):
+        return func(*args, **kwargs) * 2
+
+    return change
 
 
 class Test(Logger):
-    def recycle(self):
-        return "recycled"
+    @decorator
+    def recycle(self, arg=1):
+        return "recycled" * arg
 
 
 if __name__ == "__main__":
     a = Logger()
-    print(a.__dict__)
+    print(a)
     a.method1(2)
-    print(a.__dict__)
+    Logger.method1(Logger, 6)
+    a.method1(3)
+    Logger()()
+    Test.recycle(Test, 1)
+    print(Test.recycle)
     print()
-
-    print(LoggerTech)
+    print(Logger)
